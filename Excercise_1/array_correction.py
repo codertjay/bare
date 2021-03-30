28ca329e7c9d1de22af25a3cea6a0ef01c5fb0aa
AFENIKHENA FAVOUR <63492224+codertjay@users.noreply.github.com>
2021-03-30 15:21:43 +0100

    Update array_correction.py
    sd

--- Excercise_1/array_correction.py
+++ Excercise_1/array_correction.py
@@ -1,23 +1,20 @@
 list_item = [4,6,3,6,3,7,1]
 
+
 def get_smallest_positive_number(list_item):
     
     small_number = min(list_item)
     sort_list =sorted(list_item)
-    
-    try:
-        if list_item.count(small_number) > 1:
-            for x in sort_list:
-                try:
-                    if sort_list.count(x) == 1:
-                        return x
-                except:
-                    return small_number
-        else:
-            return small_number
-    except:
-        return -1
+   
 
-
+    if list_item.count(small_number) > 1:
+        for x in sort_list:
+            print(sort_list)
+            print(x)
+            if sort_list.count(x) == 1:
+                return x
+    else:
+        return small_number
+   
 
 get_smallest_positive_number(list_item)
