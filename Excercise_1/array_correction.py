14b41536351d4089c9124762c2021beac2a5dfed
Afenikhena Favour <codertjay@gmail.com>
2021-03-30 14:54:37 +0100

    pushing exc1

--- Excercise_1/array_correction.py
+++ Excercise_1/array_correction.py
@@ -1,5 +1,17 @@
+list_item = [4,6,3,6,3,7,1]
 
-Array1 = [1,3,6,3,2]
-Array2 = [5,-1,-3]
+def get_smallest_positive_number(list):
+    
+    small_number = min(list)
+    if list.count(small_number) > 1:
+        for x in list:
+            try:
+                if min(list.count(x)) == 1:
+                    return x
+            except:
+                return small_number
+    else :
+        return small_number
 
 
+get_smallest_positive_number(list_item)
